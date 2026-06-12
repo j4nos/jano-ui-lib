119efcc95818dda8a420835bc7dec67d317a572d
János <j4nos@users.noreply.github.com>
2026-06-12 10:25:49 +0200

    Add empty FancyFeatureFiftyOne and PPortfolioDetailsTwo top-level components

---
+++ src/components/top-level/PPortfolioDetailsTwo.tsx
@@ -0,0 +1,7 @@
+type PPortfolioDetailsTwoProps = {
+  className?: string;
+};
+
+export function PPortfolioDetailsTwo({ className }: PPortfolioDetailsTwoProps) {
+  return <div className={className} />;
+}
